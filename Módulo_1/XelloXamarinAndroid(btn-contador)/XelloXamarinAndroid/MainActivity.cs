fc7b1202b37337ff6716b00ca334d2fdf7a01467
Luis Vinicius <viniborges@yahoo.com.br>
2017-02-06 23:06:08 -0200

    Adicionado alguns exemplos do helloword do módulo 1

---
+++ Módulo_1/XelloXamarinAndroid(btn-contador)/XelloXamarinAndroid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XelloXamarinAndroid
+{
+    [Activity(Label = "XelloXamarinAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button b = FindViewById<Button>(Resource.Id.button1);
+
+            b.Click += delegate
+            {
+                b.Text = string.Format("{0} clicks", count++);
+            };
+        }
+    }
+}
+
